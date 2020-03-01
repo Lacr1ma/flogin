a1637348810088be27885310b1a5e9d07d3d1aae
Serhii Borulko <borulkosergey@icloud.com>
2020-03-01 10:41:29 +0200

    Mention dependency <routes>.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,8 @@ $EM_CONF['login'] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99'
+            'typo3' => '9.5.0-10.4.99',
+            'routes' => '*'
         ]
     ]
 ];
