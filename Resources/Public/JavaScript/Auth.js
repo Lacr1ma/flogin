94fec09c8a647da2cb1178c035e15b84e9a93de5
Serhii Borulko <borulkosergey@icloud.com>
2020-03-09 09:20:47 +0200

    [TASK] Extend login ajax form with additional magic link handlers

--- Resources/Public/JavaScript/Auth.js
+++ Resources/Public/JavaScript/Auth.js
@@ -29,6 +29,30 @@ $(function () {
         });
     });
 
+    $("#magic_form_ajax").submit(function (event) {
+        event.preventDefault();
+
+        magicLinkFormIsLoading();
+
+        const email = $(this).find('#email-field').val();
+
+        requestMagicLinkAttempt('/api/login/magic-link', email).then(function (data) {
+            if (data.redirect) {
+                $('#login_form_ajax').remove();
+                $('#magic-request-form').remove();
+                $('#notification-sent').removeClass('d-none');
+                return;
+            }
+
+            validateEmail(data.errors['email'] || '');
+
+            setTimeout(function () {
+                $('#magic_form_ajax fieldset').removeAttr('disabled');
+                $('#send-magic-link').html(TYPO3.lang['form_magic.submit']);
+            }, 200);
+        });
+    });
+
 });
 
 
@@ -74,7 +98,23 @@ const loginFormIsLoading = async () => {
 
     $('#login-button').html(`
         <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-        <span class="sr-only">${label}</span>
+        <span>${label}</span>
+    `);
+};
+
+/**
+ * Basically add loading indicator to magic link form and block it while preforming the request
+ *
+ * @return {void}
+ */
+const magicLinkFormIsLoading = async () => {
+    const label = TYPO3.lang['ajax.loading'];
+
+    $('#magic_form_ajax fieldset').attr('disabled', 'disabled');
+
+    $('#send-magic-link').html(`
+        <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+        <span>${label}</span>
     `);
 };
 
@@ -88,6 +128,20 @@ const validateUsername = async (errorMessage) => {
     let field = $('#username-field');
 
     let notice = $('.username-block > .validation');
+
+    placeError(field, notice, errorMessage);
+};
+
+/**
+ * Set validation errors for email
+ *
+ * @param {string}   errorMessage
+ * @return {void}
+ */
+const validateEmail = async (errorMessage) => {
+    let field = $('#email-field');
+
+    let notice = $('.email-block > .validation');
 
     placeError(field, notice, errorMessage);
 };
@@ -124,6 +178,19 @@ const placeError = async (field, notice, message) => {
     $(field).removeClass('is-valid').addClass('is-invalid');
 
     $(notice).text(message).removeClass('d-none').addClass('invalid-feedback');
+};
+
+/**
+ * Perform the authentication request to the BE and give back the validation response
+ *
+ * @param {string}   url             Auth endpoint
+ * @param {string}   email           User's email address
+ * @return {Object}
+ */
+const requestMagicLinkAttempt = async (url, email) => {
+    const result = await axios.post(url, {email});
+
+    return result.data;
 };
 
 /**
